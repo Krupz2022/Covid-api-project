a569ed2b5ad361480e1cc5f943a31df1a68315fd
Dricera <dricera1@gmail.com>
2022-04-10 14:46:08 +0530

    remove deprecated library dependencies and migrate to Vue3 and D3.js for main render

--- js/index.js
+++ js/index.js
@@ -1,96 +1,100 @@
-/* fetch('	https://data.covid19india.org/v4/min/data.min.json')
-    //.then((res) => res.text() )
-    //.then((res) => {
-    //    let result = res.split(/\r?\n|\r/).map(e =>{
-    //       return e.split(",")
-    //    })
-    //    console.log(result)
-    //})
+let app = Vue.createApp({
+    //...
+    data: function() {
+        return {
+            API_URL: 'https://data.covid19india.org/v4/min/data.min.json',
+            viewBoxH: 650,
+            viewBoxW: 650,
+            mapEl: undefined,
+            indiaEL: undefined,
+            container: undefined
+        }
+    },
+    methods: {
+        renderChart() {
+            let proj = d3.geoMercator()
+                .scale(1100)
+                .translate([-1250, 770]);
+            let path = d3.geoPath(proj);
 
-.then((res) => res.json())
-    .then((res) => console.log(res)) */
+            this.container = d3.select('#chart')
+                .append("svg:svg")
+                .attr("preserveAspectRatio", "xMinYMin meet")
+                .attr("viewBox", `0 0 ${this.viewBoxH} ${this.viewBoxW}`)
+                .classed("svg-content-responsive", true);
 
-var w = 600;
-var h = 650;
-var proj = d3.geo.mercator();
-var path = d3.geo.path().projection(proj);
-var t = proj.translate(); // the projection's default translation
-var s = proj.scale() // the projection's default scale
+            //let map = this.container.append("svg:g")
 
-var svg = d3.select("#chart")
-    .append("svg:svg")
-    .attr("width", w)
-    .attr("height", h)
-    .call(initialize);
+            this.indiaEl = this.container.append("svg:g")
+                .attr("id", "india")
+                .style('stroke', '#000')
+                .style('stroke-width', '0.5');
 
-var map = svg.append("svg:g")
+            // TODO refer https:/ / bl.ocks.org / almccon https: //observablehq.com/collection/@d3/
+
+            let url = "js/map.geojson";
+            let self = this;
+            Promise.resolve(d3.json(url)).then((data) => {
+                this.indiaEl.selectAll("path")
+                    .data(data.features)
+                    .enter().append("path")
+                    .attr("d", path)
+                    .attr("id", (d) => { return d.id }) //need function return to get value within enumeration
+                    .style("fill", () => {
+                        return this.getRandomColor();
+                    })
+                    .on("mouseover", function() { self.handleMouseOver(this) })
+                    .on("mouseout", function() { self.handleMouseOut(this) })
+                    .append("title")
+                    .text((d) => {
+                        return "State : " + d.id + "\n Here goes the state data";
+                    });
 
 
-var india = map.append("svg:g")
-    .attr("id", "india")
-    .style('stroke', '#000')
-    .style('stroke-width', '0.5');
+                aa = [80.9462, 26.8467];
+                // add circles to svg
+                this.indiaEl.selectAll("circle")
+                    .data([aa]).enter()
+                    .append("circle")
+                    .attr("cx", function(d) { return proj(d)[0]; })
+                    .attr("cy", function(d) { return proj(d)[1]; })
+                    .attr("r", "3px")
+                    .attr("fill", "black")
+                    .on("mouseover", function() {
+                        d3.select(this).attr("r", "6px").style('fill', "orange")
+                    })
+                    .on("mouseout", function() {
+                        d3.select(this).attr("r", "3px").style("fill", 'black')
+                    })
+                    .append("title")
+                    .text(function(d) {
+                        return "City : " + "Lucknow" + "\n Here goes the City data";
+                    })
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+            });
+
+        },
+        handleMouseOver(that) {
+            d3.select(that).attr("stroke-width", "1.5")
+        },
+        handleMouseOut(that) {
+            d3.select(that).attr("stroke-width", "0.5")
+        },
+        getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        },
+
+    },
+    computed: {
+        //
+    },
+    mounted() {
+        this.renderChart();
     }
-    return color;
-}
-
-
-
-//d3.json("data/full_data-1.json", function (json) {
-d3.json("js/map.geojson", function(json) {
-    india.selectAll("path")
-        .data(json.features)
-        .enter().append("path")
-        .attr("d", path)
-        .style("fill", function() {
-            return getRandomColor();
-        })
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut)
-        .append("title")
-        .text(function(d) {
-            return "State : " + d.id + "\n Here goes the state data";
-        });
-
-    aa = [80.9462, 26.8467];
-    // add circles to svg
-    india.selectAll("circle")
-        .data([aa]).enter()
-        .append("circle")
-        .attr("cx", function(d) { console.log(proj(d)); return proj(d)[0]; })
-        .attr("cy", function(d) { return proj(d)[1]; })
-        .attr("r", "3px")
-        .attr("fill", "black")
-        .on("mouseover", function() {
-            d3.select(this).attr("r", "6px").style('fill', "orange")
-        })
-        .on("mouseout", function() {
-            d3.select(this).attr("r", "3px").style("fill", 'black')
-        })
-        .append("title")
-        .text(function(d) {
-            return "City : " + "Lucknow" + "\n Here goes the City data";
-        })
-
 });
-
-function handleMouseOver() {
-    d3.select(this).attr("stroke-width", "1.4")
-}
-
-function handleMouseOut() {
-    d3.select(this).attr("stroke-width", "0.5")
-}
-
-
-
-function initialize() {
-    proj.scale(6700);
-    proj.translate([-1240, 750]);
-}
+app.mount('#app');
